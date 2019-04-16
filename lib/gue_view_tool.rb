7b0ed5fc09e0ef3bb493cd98d5f16f6241510a1d
doubleg <darnel_free@yahoo.com>
2019-04-16 13:30:02 -0500

    gue tool initial commit

---
+++ lib/gue_view_tool.rb
@@ -0,0 +1,8 @@
+require "gue_view_tool/version"
+require "gue_view_tool/renderer"
+
+
+module GueViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/gue_view_tool_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe GueViewTool do
+  it "has a version number" do
+    expect(GueViewTool::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
